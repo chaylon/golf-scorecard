a1ff781094f93f56d5af53b2851d82b494ade9d7
chaylon <chaylon@fandm.edu>
2017-02-23 16:43:33 -0500

    form creates holes

--- app/controllers/api/v1/holes_controller.rb
+++ app/controllers/api/v1/holes_controller.rb
@@ -1,23 +1,16 @@
 class Api::V1::HolesController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+
   def create
     holeInfo = params["courseData"]
     holeInfo.each do |hole|
-      binding.pry
       Hole.create(
-        number: params["hole"].to_i,
-        par: params["par"].to_i,
-        yardage: params["yardage"].to_i,
+        number: hole["hole"].to_i,
+        par: hole["par"].to_i,
+        yardage: hole["yardage"].to_i,
         course_id: params["course_id"].to_i,
       )
     end
   end
-
-  private
-
-  def hole_params
-    params.require(:hole).permit(:number, :par, :yardage, :course_id, :courseInfo)
-  end
-
 end
