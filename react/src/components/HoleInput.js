c010571f3b3def5e2c9c0fc40a301ba87f215c83
chaylon <chaylon@fandm.edu>
2017-03-02 16:00:57 -0500

    default hole values

--- react/src/components/HoleInput.js
+++ react/src/components/HoleInput.js
@@ -3,7 +3,7 @@ import React from 'react';
 const HoleInput = props => {
   return(
     <div>
-      <input type="text" placeholder="Hole"/>
+      <input type="text" defaultValue={props.id}/>
       <input type="text" placeholder="Par"/>
       <input type="text" placeholder="Yardage"/>
     </div>
