23c8e329677ec2613e81a9256231c220e31cc5b6
chaylon <chaylon@fandm.edu>
2017-03-21 14:55:27 -0400

    profile page has round info

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,7 +6,14 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def show
-
+    scorecards = Scorecard.where("user_id = #{current_user.id}")
+    rounds = scorecards.length.to_f
+    sum = 0
+    scorecards.each do |scorecard|
+      sum += scorecard.total
+    end
+    average = sum/rounds
+    render json: {rounds: rounds, average: average}
   end
 
 end
--- react/src/components/User.js
+++ react/src/components/User.js
@@ -3,12 +3,36 @@ import React, {Component} from 'react';
 class User extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      rounds: 0,
+      average: 0
+    };
+    this.getInfo = this.getInfo.bind(this);
+  }
+
+  componentDidMount() {
+    this.getInfo();
+  }
+
+  getInfo() {
+    fetch(`/api/v1/users/${this.props.params.id}`,
+      {credentials: "same-origin"}
+    )
+    .then(response => response.json())
+    .then(body => {
+      this.setState({
+        rounds: body.rounds,
+        average: body.average
+      })
+    });
   }
 
   render() {
+
     return(
       <div>
-        Hello
+        <p>Rounds: {this.state.rounds}</p>
+        <p>Average Score: {this.state.average}</p>
       </div>
     );
   }
